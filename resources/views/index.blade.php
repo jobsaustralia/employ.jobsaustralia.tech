d972dff30b638892bc30cd511a71400d30417828
kimluu1701 <kimmie@Kims-MacBook-Pro.local>
2017-08-17 16:19:44 +1000

    Merging two repos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,17 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+
+
+        <!-- Panel 1 -->
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome!</div>
+                <div class="panel-heading"> <i class="fa fa-briefcase" aria-hidden="true" style="font-size: 30px"></i> Welcome!</div>
 
                 <div class="panel-body">
-                    The quick brown fox jumps over the lazy dog.
+                    <p> Jobsaustralia.tech is a job-matching website based in Melbourne, Australia focusing in the field of Information Technology, Computer Science and Software Engineering.</p>
                 </div>
             </div>
+            
         </div>
     </div>
 </div>
