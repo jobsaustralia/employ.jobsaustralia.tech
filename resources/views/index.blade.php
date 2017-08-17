86ca4f9c35b1f28d9d135ca0b71bf36178c0ef85
kimluu1701 <kimmie@Kims-MacBook-Pro.local>
2017-08-17 16:38:59 +1000

    Homepage E

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,30 @@
                     <p> Jobsaustralia.tech is a job-matching website based in Melbourne, Australia focusing in the field of Information Technology, Computer Science and Software Engineering.</p>
                 </div>
             </div>
+
+        <!-- Panel 2 -->
+            <div class="panel panel-default">
+                <div class="panel-heading"> <i class="fa fa-users" aria-hidden="true" style="font-size: 30px"></i> We're Here For You</div>
+
+                <div class="panel-body">
+                    <p> Help you to get access to a niche audience, meaning less irrelevant resumes clogging up your inbox.</p>
+                    <p> Match your current job vacancies to the potential employees.</p>
+                    <p> Connect you with the job seekers that are the most suitable for your company culture.</p>
+                    <p> Market you to employees and the society.</p>
+                </div>
+            </div>
+
+            <!-- Panel 3 -->
+            <div class="panel panel-default">
+                <div class="panel-heading"><i class="fa fa-compass" aria-hidden="true" style="font-size: 30px"></i> The Guideline</div>
+                <div class="panel-body">
+                <p> <b>Step 1:</b> Register yourself with jobaustralia.tech. </p>
+                <p> <b>Step 2:</b> Creat your own profile.</p>
+                <p> <b>Step 3:</b> Start list your job vancies. It is recommended that you pay high attention to listing your required qualifications, skills and experiences.
+                </p>
+                <p> <b>Step 4:</b> Wait for your notifications whenever we find you a great match with job seekers ! </p>
+                </div>
+            </div> 
             
         </div>
     </div>
