9ec35f346bfde1ac868d654208d24e7428ed998d
d473 <30338371+d473@users.noreply.github.com>
2017-08-18 12:46:13 +1000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,22 @@
                 <div class="panel-body">
                     You are logged in!
                 </div>
+                <h4><a>Post A Job</a></h4>
+                <h4><a>View applicants</a></h4>
+                <h4><a>Edit your profile</a></h4>
             </div>
         </div>
     </div>
+    +   <!--footer-->
+ +            <div id="footer">
+ +                <ul class="footernav">
+ +		<li><a href="{{ url('/gettingStarted') }}">Getting Started</a></li>
+ +                 <li><a href="{{ url('/aboutus') }}">About Us</a></li>
+ +                </ul>
+ +                <h5 class="footer">© 2017 </h5>
+ +                <h5 class="footer">This Site has been made for educational purposes by students of RMIT University for the Capstone Programing Project</h5>
+ +                </div>
+ +                <!--//footer--> 
+ +    </div>  
 </div>
 @endsection
