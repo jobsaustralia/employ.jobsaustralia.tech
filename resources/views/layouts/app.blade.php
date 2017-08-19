3f350b52a45d0281b384d4977a2eea84b20f5b8d
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-19 17:44:00 +1000

    Fixed CSS header and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,14 +15,6 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id="header">
-        <div id="topBar"> 
-            <ul class="toptitle">
-                <li><h1>jobsaustralia.tech</h1></li>
-            </ul>
-            </div>
-         </div>
-          <!--//header--> 
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
@@ -88,15 +80,9 @@
         @yield('content')
         
    <!--footer-->
-            <div id="footer">
-                <ul class="footernav">
-		<li><a href="{{ url('/gettingStarted') }}">Getting Started</a></li>
-                 <li><a href="{{ url('/aboutus') }}">About Us</a></li>
-                </ul>
-                <h5 class="footer">© 2017 </h5>
-                <h5 class="footer">This Site has been made for educational purposes by students of RMIT University for the Capstone Programing Project</h5>
-                </div>
-                <!--//footer--> 
+        <footer>
+            <p>Copyright © JobsAustralia.tech 2017 &bull; <a href="https://github.com/jobsaustralia/">GitHub</a> &bull; <a href="LICENSE">License</a></p>
+        </footer>
     </div>                       
          <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
