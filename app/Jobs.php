dabdcdb02e6e4bda8d70a371c2b8f6f067b5a153
kimluu1701 <kim.possiblee1701@gmail.com>
2017-08-22 15:32:25 +1000

    Jobs model

---
+++ app/Jobs.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'description', 'hours', 'salary', 'availablefrom', 'location', 'startdate', 'java', 'python', 'c', 'csharp', 'cplus', 'php', 'html', 'css', 'javascript', 'sql', 'unix', 'winserver', 'windesktop', 'linuxdesktop', 'macosdesktop', 'pearl', 'bash', 'batch', 'cisco', 'office', 'r', 'go', 'ruby', 'asp', 'scala'];
+
+    protected $table = 'employers';
+}
