05bfd55f533a39bc53942aefb397dc10b0f407c3
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-25 20:33:34 +1000

    Pointed to correct route, fixed HTML issues.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,9 +5,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">	Contact Us</div>
+                <div class="panel-heading"> Contact Us</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal" method="POST" action="{{ route('enquire') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
@@ -23,8 +23,8 @@
                                 @endif
                             </div>
                         </div>
-						
-						<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">Email</label>
 
                             <div class="col-md-6">
@@ -37,27 +37,13 @@
                                 @endif
                             </div>
                         </div>
-						
-						<div class="form-group{{ $errors->has('company') ? ' has-error' : '' }}">
-                            <label for="company" class="col-md-4 control-label">Company</label>
-
-                            <div class="col-md-6">
-                                <input id="company" type="text" class="form-control" name="company" value="{{ old('company') }}" required autofocus>
-
-                                @if ($errors->has('company'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('company') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
 
                         <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
                             <label for="message" class="col-md-4 control-label">Message</label>
 
                             <div class="col-md-6">
-									<textarea id="message" name="message" rows="5" cols="30" class="form-control" value="{{ old('message') }}" required autofocus>
-									</textarea>
+                                    <textarea id="message" name="message" rows="5" cols="30" class="form-control" value="{{ old('message') }}" required autofocus>
+                                    </textarea>
                                 @if ($errors->has('message'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('message') }}</strong>
