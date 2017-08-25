3f29d1af12d76233688f07a60cd4d0849ac95b08
Aaron Horler <aghorler@users.noreply.github.com>
2017-08-25 20:32:33 +1000

    Moved everything to single function

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div id="profile" class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ Auth::user()->name }}'s Employer Profile</div>
 
