7376f8c7e81804ce65aaf6569d9045bb091d82cb
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-13 18:44:22 +1000

    Cosmetic changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,41 +4,47 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-
-
-            <!-- Panel 1 -->
             <div class="panel panel-default">
-                <div class="panel-heading"> <i class="fa fa-briefcase" aria-hidden="true" style="font-size: 30px"></i><b> WELCOME!</b></div>
+                <div class="panel-heading"><strong>Welcome</strong></div>
 
-                <div class="panel-body">
-                    <p> Jobsaustralia.tech is a job-matching website based in Melbourne, Australia focusing in the field of Information Technology, Computer Science and Software Engineering.</p>
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-briefcase" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p>JobsAustralia.tech is a job matchmaking website based in Melbourne, Australia focusing in the fields of <strong>Information Technology</strong>, <strong>Computer Science</strong>, and <strong>Software Engineering</strong>.</p><br>
+                    <p><a class="btn btn-primary" @if (Auth::guest()) href="{{ route('register') }}">Register now @else href="{{ route('matches') }}">View matches @endif</a></p>
                 </div>
             </div>
 
-            <!-- Panel 2 -->
             <div class="panel panel-default">
-                <div class="panel-heading"> <i class="fa fa-users" aria-hidden="true" style="font-size: 30px"></i><b> WE'RE HERE FOR YOU </b></div>
+                <div class="panel-heading"><strong>Algorithmic matchmaking</strong></div>
 
-                <div class="panel-body">
-                    <p> Help you to get access to a niche audience, meaning less irrelevant resumes clogging up your inbox.</p>
-                    <p> Match your current job vacancies to the potential employees.</p>
-                    <p> Connect you with the job seekers that are the most suitable for your company culture.</p>
-                    <p> Market you to employees and the society.</p>
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-calculator" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p>We use an advanced matchmaking algorithm to match job seekers to your job advertisements.</p>
+                    <p>After you register, and job seekers apply to your posted job advertisements, matching applicants are listed in order by a <strong>0% to 100%</strong> match depending on the requirements of your job and their self-reported skillset.</p>
                 </div>
             </div>
 
-            <!-- Panel 3 -->
             <div class="panel panel-default">
-                <div class="panel-heading"><i class="fa fa-compass" aria-hidden="true" style="font-size: 30px"></i><b> THE GUIDELINE </b></div>
-                <div class="panel-body">
-                <p> <b>Step 1:</b> Register yourself with jobaustralia.tech. </p>
-                <p> <b>Step 2:</b> Creat your own profile.</p>
-                <p> <b>Step 3:</b> Start list your job vancies. It is recommended that you pay high attention to listing your required qualifications, skills and experiences.
-                </p>
-                <p> <b>Step 4:</b> Wait for your notifications whenever we find you a great match with job seekers ! </p>
+                <div class="panel-heading"><strong>We're here for you</strong></div>
+
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-users" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p>&bull; Help you to get access to a niche audience, meaning less irrelevant resumes clogging up your inbox.</p>
+                    <p>&bull; Match your current job vacancies to the potential employees.</p>
+                    <p>&bull; Connect you with the job seekers that are the most suitable for your company culture.</p>
+                    <p>&bull; Market you to employees and the society.</p>
                 </div>
-            </div> 
-            
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong>The guideline</strong></div>
+                <div class="panel-body" align="center">
+                    <p><i class="fa fa-compass" aria-hidden="true" style="font-size: 100px"></i></p>
+                    <p><strong>Step 1:</strong> Register yourself with JobsAustralia.tech</p>
+                    <p><strong>Step 2:</strong> Creat your own profile.</p>
+                    <p><strong>Step 3:</strong> List your job vancies. It is recommended that you pay high attention to listing your required skills - we use this to perform matchmaking.</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -8,19 +8,19 @@
             <div class="panel panel-default">
                 <div class="panel-heading">	Setting An Account</div>
                 <div class="panel-body">
-                    <p><b>I forgot my password. What could I do ?</b></p>
-                    <p> Don't worry, it happens to everyone. Click the ‘Forgot Your Password ?’ link at the sign in section to enter your email and we'll send you a link to reset your password.</p><br>
-                    <p><b>How do I update my information ?</b></p>
-                    <p> Once you have logged in to your account, click on the drop down arrow next to your name in the top right of the page, and select Settings.</p>
+                    <p><strong>I forgot my password. What could I do ?</strong></p>
+                    <p>Don't worry, it happens to everyone. Click the ‘Forgot Your Password?’ link at the sign in section to enter your email and we'll send you a link to reset your password.</p><br>
+                    <p><strong>How do I update my information ?</strong></p>
+                    <p>Once you have logged in to your account, click on the drop down arrow next to your name in the top right of the page, and select Settings.</p>
                 </div>
             </div>
 
             <div class="panel panel-default">
                 <div class="panel-heading"> Job Posting</div>
                 <div class="panel-body">
-                    <p><b>I am a new user. How do I post an ad ?</b></p>
+                    <p><strong>I am a new user. How do I post an ad ?</strong></p>
                     <p> Before posting your jobs, you will need to create an Employer account with a username and password.</p><br>
-                    <p><b>How do I update my information ?</b></p>
+                    <p><strong>How do I update my information ?</strong></p>
                     <p> Your ad will appear online right after you finish updating</p>
                 </div>
             </div>
@@ -28,12 +28,13 @@
             <div class="panel panel-default">
             <div class="panel-heading"> Technical Issues</div>
                 <div class="panel-body">
-                    <p><b>Getting an error message or having trouble loading a page?</b></p>
-                    <p> Try clearing your browsing history following <a href="https://kb.iu.edu/d/ahic">these instructions</a></p>
-                    <p> Also, ensure your <a href="https://updatemybrowser.org/">browser is up to date</a>.</p><br>
-                    <p><b>How could I enable my Javascript ?</b></p>
-                    <p> Follow <a href="http://www.enable-javascript.com/">these instructions</a> to enable your Javascript.</p></br>
-                    <p><b>Tried everything and still having trouble?</b></p>
+                    <p><strong>Getting an error message or having trouble loading a page?</strong></p>
+                    <p>Try clearing your browsing history following <a href="https://kb.iu.edu/d/ahic">these instructions</a></p>
+                    <p>Also, ensure your <a href="https://updatemybrowser.org/">browser is up to date</a>.</p><br>
+                    <p><strong>How can I enable JavaScript ?</strong></p>
+                    <p>JobsAustralia.tech requires JavaScript to perform matchmaking, and various other site functions.</p>
+                    <p>Follow <a href="http://www.enable-javascript.com/">these instructions</a> to enable JavaScript.</p></br>
+                    <p><strong>Tried everything and still having trouble?</strong></p>
                     <p><a href="contact">Contact us</a> to let us know what is going on. Be sure to include as much information about the issue as you can, including: your operating system, web browser and version, anti-virus software you have installed and the page that you encountered the error on.</p>
                 </div>
             </div>
