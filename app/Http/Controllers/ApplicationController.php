3a26cc8a69ef57e27e99a8aff0e04199d5d4907c
rmit-s3491115-ozlem-kirmizi <s3491115@student.rmit.edu.au>
2017-09-19 17:54:32 +1000

    Create application.blade.php + display application view in ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Job;
 use App\User;
+use App\Application;
+use App\JobSeeker;
 
 use Auth;
 
@@ -30,5 +32,49 @@ class ApplicationController extends Controller{
         else{
             return Redirect::route('jobs');
         }
+    }
+	
+	/* Display application view. */
+    public function displayApplication($id){
+		$employer = Auth::user();
+		$application = Application::findOrFail($id);
+		$user = JobSeeker::findOrFail($application->userid);
+		
+		$name = $user->name;
+		$email = $user->email;
+		$title = $user->title;
+		$sector = $user->sector;
+		$experience = $user->experience;
+		$state = $user->state;
+		$city = $user->city;
+		$java = $user->java;
+		$python = $user->python;
+		$c = $user->c;
+		$csharp = $user->csharp;
+		$cplus = $user->cplus;
+		$php = $user->php;
+		$html = $user->html;
+		$css = $user->css;
+		$javascript = $user->javascript;
+		$sql = $user->sql;
+		$unix = $user->unix;
+		$winserver = $user->winserver;
+		$windesktop = $user->windesktop;
+		$linuxdesktop = $user->linuxdesktop;
+		$macosdesktop = $user->macosdesktop;
+		$pearl = $user->pearl;
+		$bash = $user->bash;
+		$batch = $user->batch;
+		$cisco = $user->cisco;
+		$office = $user->office;
+		$r = $user->r;
+		$go = $user->go;
+		$ruby = $user->ruby;
+		$asp = $user->asp;
+		$scala = $user->scala;
+		$message = $application->message;
+		return view('application', ["name"=>$name, "email"=>$email, "title"=>$title, "sector"=>$sector, "experience"=>$experience,
+		"state"=>$state, "city"=>$city, "java"=>$java, "python"=>$python, "c"=>$c, "csharp"=>$csharp, "cplus"=>$cplus, "php"=>$php, "html"=>$html, "css"=>$css, "javascript"=>$javascript, "sql"=>$sql, "unix"=>$unix, "winserver"=>$winserver, "windesktop"=>$windesktop,
+		"linuxdesktop"=>$linuxdesktop, "macosdesktop"=>$macosdesktop, "pearl"=>$pearl, "bash"=>$bash, "batch"=>$batch, "cisco"=>$cisco, "office"=>$office, "r"=>$r, "go"=>$go, "ruby"=>$ruby, "asp"=>$asp, "scala"=>$scala, "message"=>$message]);
     }
 }
