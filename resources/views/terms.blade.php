1fe1b395855500c03f80ca2cbd0c415ddb74c2ec
kimluu1701 <kim.possiblee1701@gmail.com>
2017-09-20 17:27:14 +1000

    Categorise skills

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -4,7 +4,14 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h3><i class="fa fa-gavel" aria-hidden="true"></i> Legal - License, Terms, and Privacy Policy.</h3><br>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                <h3><i class="fa fa-info-circle" aria-hidden="true"></i> Legal</h3>
+                <p>JobsAustralia.tech is published with a license, terms of use, and a privacy policy.</p>
+                <p>Please read this page before using the service.</p>
+                </div>
+            </div>
+            
             <div class="panel panel-default">
                 <div class="panel-heading">License</div>
                 <div class="panel-body">
@@ -13,37 +20,37 @@
                     <p>Copyright (c) 2017 Aaron Horler, Dennis Mihalache, Kim Luu, Melissa Nguyen, and Ozlem Kirmizi (as JobsAustralia.tech at RMIT University).</p>
 
                     <p>Permission is hereby granted, free of charge, to any person obtaining a copy
-                    of this software and associated documentation files (the "Software"), to deal
-                    in the Software without restriction, including without limitation the rights
-                    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                    copies of the Software, and to permit persons to whom the Software is
-                    furnished to do so, subject to the following conditions:</p>
+                        of this software and associated documentation files (the "Software"), to deal
+                        in the Software without restriction, including without limitation the rights
+                        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                        copies of the Software, and to permit persons to whom the Software is
+                        furnished to do so, subject to the following conditions:</p>
 
-                    <p>The above copyright notice and this permission notice shall be included in all
-                    copies or substantial portions of the Software.</p>
+                        <p>The above copyright notice and this permission notice shall be included in all
+                            copies or substantial portions of the Software.</p>
 
-                    <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                    SOFTWARE.</p>
+                            <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                                IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                                FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                                LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                                OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                                SOFTWARE.</p>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Terms of Use</div>
+                            <div class="panel-body">
+                                <p>This is yet to be populated. JobsAustrlia.tech is still in development. We advise against using it.</p>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Privacy Policy</div>
+                            <div class="panel-body">
+                                <p>This is yet to be populated. JobsAustrlia.tech is still in development. We advise against using it.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">Terms of Use</div>
-                <div class="panel-body">
-                    <p>This is yet to be populated. JobsAustrlia.tech is still in development. We advise against using it.</p>
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">Privacy Policy</div>
-                <div class="panel-body">
-                    <p>This is yet to be populated. JobsAustrlia.tech is still in development. We advise against using it.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+            @endsection
