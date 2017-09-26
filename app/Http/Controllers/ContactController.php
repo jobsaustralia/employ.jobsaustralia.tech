5b512aa22ea6900deccf1a6bfb6c9dae479cce71
rmit-s3491115-ozlem-kirmizi <s3491115@student.rmit.edu.au>
2017-09-26 15:46:05 +1000

    Initial works on contact form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Mail;
 
 class ContactController extends Controller{
-    public function send(Request $request){ 
-        // Nothing here yet.
+
+    /* Send an email. */
+    function send(Request $request){
+        Mail::raw($request['message'], function($message){
+        $message->subject('This is a test.');
+        $message->to('s3491115@student.rmit.edu.au');
+		});
     }
 }
