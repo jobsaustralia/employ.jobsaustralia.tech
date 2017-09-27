e576ce0a1c8ad02bd7b0c31059b051fe3b91e37a
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-27 23:51:20 +1000

    Preliminary changes to notification settings.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,8 +10,7 @@
                 <div class="panel-body">
                     <p><strong>Name:</strong> {{ Auth::user()->name }}</p>
                     <p><strong>Email:</strong> {{ Auth::user()->email }}</p>
-                    <p><strong>State:</strong> {{ Auth::user()->state }}</p>
-                    <p><strong>City:</strong> {{ Auth::user()->city }}</p>
+                    <p><strong>Location:</strong> {{ Auth::user()->city }}, @if (Auth::user()->state == "vic") Victoria @elseif (Auth::user()->state == "nsw") New South Wales @elseif (Auth::user()->state == "qld") Queensland @elseif (Auth::user()->state == "wa") Western Australia @elseif (Auth::user()->state == "sa") South Australia @elseif (Auth::user()->state == "tas") Tasmania @elseif (Auth::user()->state == "act") Australian Capital Territory @elseif (Auth::user()->state == "nt") Northern Territory @elseif (Auth::user()->state == "oth") Other Australian Region @endif</p>
                     <hr>
 
                     <p>
@@ -59,31 +58,59 @@
                     </form>
                 </div>
             </div>
-			
-			<div class="panel panel-default">
+            
+            <div class="panel panel-default">
                 <div class="panel-heading">Notifications</div>
 
                 <div class="panel-body">
-                    <p>Please tick the email notifications you would like to receive.</p>
-						<hr>
-						<form class="form-horizontal" method="POST" enctype="multipart/form-data" action="{{ route('editProfile') }}">
+                    <p>Please select the email notifications you would like to receive.</p>
+
+                    <hr>
+
+                    <form class="form-horizontal" method="POST" enctype="multipart/form-data" action="{{ route('profile') }}">
                         {{ csrf_field() }}
-						<div class="form-group{{ $errors->has('applied') ? ' has-error' : '' }}">
-                            <label for="applied" class="col-md-4 control-label">Notify me when someone applies to a job</label>
+
+                        <div class="form-group{{ $errors->has('applied') ? ' has-error' : '' }}">
+                            <label for="applied" class="col-md-4 control-label">Notify me when a job seeker applies to my job</label>
 
                             <div class="col-md-1">
                                 <input id="applied-hidden" type="hidden" class="form-control" name="applied" value="0">
                                 <input id="applied" type="checkbox" class="form-control" name="applied" value="{{ old('applied', 1) }}">
-                            
+
                                 @if ($errors->has('applied'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('applied') }}</strong>
                                     </span>
                                 @endif
                             </div>
-                      </div>
-					 </div></div>
-			</form>		
+                        </div>
+                        
+                        
+                        <div class="form-group{{ $errors->has('marketing') ? ' has-error' : '' }}">
+                            <label for="marketing" class="col-md-4 control-label">Send me occasional marketing emails</label>
+
+                            <div class="col-md-1">
+                                <input id="marketing-hidden" type="hidden" class="form-control" name="marketing" value="0">
+                                <input id="marketing" type="checkbox" class="form-control" name="marketing" value="{{ old('marketing', 1) }}">
+
+                                @if ($errors->has('marketing'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('marketing') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Save Changes
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
