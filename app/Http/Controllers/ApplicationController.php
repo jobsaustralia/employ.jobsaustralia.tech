3d31b36dad73a41a70a229932ad4788a248dad10
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-30 17:12:40 +1000

    Add new skills to Application view.

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -63,7 +63,7 @@ class ApplicationController extends Controller{
         $windesktop = $user->windesktop;
         $linuxdesktop = $user->linuxdesktop;
         $macosdesktop = $user->macosdesktop;
-        $pearl = $user->pearl;
+        $perl = $user->perl;
         $bash = $user->bash;
         $batch = $user->batch;
         $cisco = $user->cisco;
@@ -74,11 +74,36 @@ class ApplicationController extends Controller{
         $asp = $user->asp;
         $scala = $user->scala;
         $cow = $user->cow;
+        $actionscript = $user->actionscript;
+        $assembly = $user->assembly;
+        $autohotkey = $user->autohotkey;
+        $coffeescript = $user->coffeescript;
+        $d = $user->d;
+        $fsharp = $user->fsharp;
+        $haskell = $user->haskell;
+        $matlab = $user->matlab;
+        $objectivec = $user->objectivec;
+        $objectivecplus = $user->objectivecplus;
+        $pascal = $user->pascal;
+        $powershell = $user->powershell;
+        $rust = $user->rust;
+        $swift = $user->swift;
+        $typescript = $user->typescript;
+        $vue = $user->vue;
+        $webassembly = $user->webassembly;
+        $apache = $user->apache;
+        $aws = $user->aws;
+        $docker = $user->docker;
+        $nginx = $user->nginx;
+        $saas = $user->saas;
+        $ipv4 = $user->ipv4;
+        $ipv6 = $user->ipv6;
+        $dns = $user->dns;
         $message = $application->message;
         $jobid = $job->id;
         $userid = $user->id;
         $github = $user->github;
 
-        return view('application', ["name"=>$name, "email"=>$email, "title"=>$title, "sector"=>$sector, "experience"=>$experience, "state"=>$state, "city"=>$city, "java"=>$java, "python"=>$python, "c"=>$c, "csharp"=>$csharp, "cplus"=>$cplus, "php"=>$php, "html"=>$html, "css"=>$css, "javascript"=>$javascript, "sql"=>$sql, "unix"=>$unix, "winserver"=>$winserver, "windesktop"=>$windesktop, "linuxdesktop"=>$linuxdesktop, "macosdesktop"=>$macosdesktop, "pearl"=>$pearl, "bash"=>$bash, "batch"=>$batch, "cisco"=>$cisco, "office"=>$office, "r"=>$r, "go"=>$go, "ruby"=>$ruby, "asp"=>$asp, "scala"=>$scala, "cow"=>$cow, "message"=>$message, "jobid"=>$jobid, "github"=>$github, "userid"=>$userid]);
+        return view('application', ["name"=>$name, "email"=>$email, "title"=>$title, "sector"=>$sector, "experience"=>$experience, "state"=>$state, "city"=>$city, "java"=>$java, "python"=>$python, "c"=>$c, "csharp"=>$csharp, "cplus"=>$cplus, "php"=>$php, "html"=>$html, "css"=>$css, "javascript"=>$javascript, "sql"=>$sql, "unix"=>$unix, "winserver"=>$winserver, "windesktop"=>$windesktop, "linuxdesktop"=>$linuxdesktop, "macosdesktop"=>$macosdesktop, "perl"=>$perl, "bash"=>$bash, "batch"=>$batch, "cisco"=>$cisco, "office"=>$office, "r"=>$r, "go"=>$go, "ruby"=>$ruby, "asp"=>$asp, "scala"=>$scala, "cow"=>$cow, "message"=>$message, "jobid"=>$jobid, "github"=>$github, "userid"=>$userid, "actionscript"=>$actionscript, "assembly"=>$assembly, "autohotkey"=>$autohotkey, "coffeescript"=>$coffeescript, "d"=>$d, "fsharp"=>$fsharp, "haskell"=>$haskell, "matlab"=>$matlab, "objectivec"=>$objectivec, "objectivecplus"=>$objectivecplus, "pascal"=>$pascal, "powershell"=>$powershell, "rust"=>$rust, "swift"=>$swift, "typescript"=>$typescript, "vue"=>$vue, "webassembly"=>$webassembly, "apache"=>$apache, "aws"=>$aws, "docker"=>$docker, "nginx"=>$nginx, "saas"=>$saas, "ipv4"=>$ipv4, "ipv6"=>$ipv6, "dns"=>$dns]);
     }
 }
