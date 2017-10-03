a698f820f4e57aaa4c735b604354f64449af3737
Aaron Horler <aghorler@users.noreply.github.com>
2017-10-03 17:31:21 +1100

    Enforce session encryption.

--- config/session.php
+++ config/session.php
@@ -45,7 +45,7 @@ if(isset($_SERVER['SERVER_NAME']) &&  $_SERVER['SERVER_NAME'] == "employ.jobsaus
         |
         */
 
-        'encrypt' => false,
+        'encrypt' => true,
 
         /*
         |--------------------------------------------------------------------------
